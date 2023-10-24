5f72d538995a7425ea04d1f6964e2f5f9c1740a2
JuniorAlfredoHuerta <44213192+JuniorAlfredoHuerta@users.noreply.github.com>
2023-10-24 10:34:19 -0500

    implementación de tesxprocess

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from flask import jsonify
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import torch
 import librosa
-
+from textprocess import recibirjson
 app = Flask(__name__)
-
 model = Wav2Vec2ForCTC.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
 model.load_state_dict(torch.load('./model.pth'))
 processor = Wav2Vec2Processor.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
-
 #input_audio, _ = librosa.load(audio3, sr=16000)
 #inputs = processor(input_audio, sampling_rate=16_000, return_tensors="pt", padding=True)
 #with torch.no_grad():
@@ -17,8 +15,6 @@ processor = Wav2Vec2Processor.from_pretrained("jonatasgrosman/wav2vec2-large-xls
 #predicted_ids = torch.argmax(logits, dim=-1)
 #predicted_sentences = processor.batch_decode(predicted_ids)
 #print("Prediction:", predicted_sentences)
-
-
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     audio = request.files['audio']
@@ -29,7 +25,6 @@ def transcribe():
         logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
     predicted_ids = torch.argmax(logits, dim=-1)
     predicted_sentences = processor.batch_decode(predicted_ids)
-    return {"transcription": predicted_sentences[0]}
-
+    return {"transcription": recibirjson(predicted_sentences[0])}
 if __name__ == '__main__':
     app.run(debug=True)
