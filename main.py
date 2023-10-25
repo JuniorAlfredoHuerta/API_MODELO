7fcd9b1bc62a5d2ad499876064b8c9eb545cba56
JuniorAlfredoHuerta <44213192+JuniorAlfredoHuerta@users.noreply.github.com>
2023-10-25 01:34:47 -0500

    Transcription por frontend

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
-from flask import Flask, request
-from flask import jsonify
+from flask import Flask, request, jsonify
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import torch
 import librosa
 from textprocess import recibirjson
+from flask_cors import CORS  # Import CORS
+
+
 app = Flask(__name__)
+CORS(app, resources={r"/transcribe": {"origins": "http://localhost:3000"}})
+
 model = Wav2Vec2ForCTC.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
 model.load_state_dict(torch.load('./model.pth'))
 processor = Wav2Vec2Processor.from_pretrained("jonatasgrosman/wav2vec2-large-xlsr-53-spanish")
-#input_audio, _ = librosa.load(audio3, sr=16000)
-#inputs = processor(input_audio, sampling_rate=16_000, return_tensors="pt", padding=True)
-#with torch.no_grad():
-#    logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
-#predicted_ids = torch.argmax(logits, dim=-1)
-#predicted_sentences = processor.batch_decode(predicted_ids)
-#print("Prediction:", predicted_sentences)
-@app.route('/transcribe', methods=['POST'])
+
+@app.route('/transcribe', methods=['POST', 'GET'])
 def transcribe():
-    audio = request.files['audio']
-    audio.save('audio.wav')
-    input_audio, _ = librosa.load('audio.wav', sr=16000)
+    audio = request.files['audio'] # RECIBO DEL FRONT ( POR COMANDO UN WAV)
+
+    input_audio,_   = librosa.load(audio, sr=16000)
     inputs = processor(input_audio, sampling_rate=16_000, return_tensors="pt", padding=True)
     with torch.no_grad():
         logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
     predicted_ids = torch.argmax(logits, dim=-1)
     predicted_sentences = processor.batch_decode(predicted_ids)
-    return {"transcription": recibirjson(predicted_sentences[0])}
+    print(predicted_sentences)
+    return jsonify({"transcription": recibirjson(predicted_sentences[0])})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
